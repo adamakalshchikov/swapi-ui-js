42560d11159203760128d570f47a1ef9de70f65b
Aleksandr Damakalshchikov <a.damakalshchikov@corp.mail.ru>
2021-07-22 00:51:23 +0300

    lsn1 added event handlers for preview, menu, theme selector

--- js/main.js
+++ js/main.js
@@ -6,3 +6,31 @@ $(function(){
         $(this).load(file);
     });
 });
+
+$('.menu-btn').on('click', function () {
+    $('.menu-wrapper').slideToggle('.slow');
+});
+
+let editPopup = document.querySelector('.edit-popup');
+let editPopupSwitcher = false;
+
+$('#edit').on('click', function () {
+    this.classList.remove('pulse');
+    editPopupSwitcher = !editPopupSwitcher;
+    if (editPopupSwitcher) {
+        TweenMax.to(editPopup, 1, {x: '2%', display: 'block', height: '65vh', ease: Back.easeOut});
+    } else {
+        TweenMax.to(editPopup, 1, {x: '-200%', display: 'block', height: '0vh', ease: Power3.easeOut});
+    }
+});
+
+let preview = $('.preview-wrapper');
+let themeSelects = document.querySelectorAll('.theme-select');
+
+
+$('.theme-select').on('click', function(){
+    for (let el of themeSelects){
+        el.classList.remove('selected-theme');
+        this.classList.add('selected-theme');
+    }
+});
